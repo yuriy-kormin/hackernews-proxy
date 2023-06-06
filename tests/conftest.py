d5b9aa00718368a2f1ce4a535a42149188c298c7
yuriy.kormin <yuriy.kormin@gmail.com>
2023-06-06 12:21:09 +0400

    tests for href modification

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,32 @@
 import pytest
 from app.app import app
-# from quart.testing import make_test_client
 
-# @pytest.fixture
-# def client():
-#     # app.config["TESTING"] = True
-#     return app.test_client()
-# yield client
-# @pytest.mark.asyncio
-# @pytest.fixture
-# async def create_test_app():
+FAKE_HTML = (
+    'tests/fixtures/root_page.html',
+    'tests/fixtures/root_page_correct_result.html'
+)
+
+FAKE_HTML_HREF = (
+    'tests/fixtures/root_page_with_symlinks.html',
+    'tests/fixtures/root_page_with_symlinks_result.html'
+)
 
 
 @pytest.fixture
 def client():
-    # async with app.test_client() as client:
     return app.test_client()
 
 
 @pytest.fixture
 def get_qa_root_html():
-    return tuple(
-        get_fixture_content(path) for path in
-        (FAKE_HTML, FAKE_HTML_RESULT)
-    )
+    return tuple(get_fixture_content(path) for path in FAKE_HTML)
+
+
+@pytest.fixture
+def get_qa_href_html():
+    return tuple(get_fixture_content(path) for path in FAKE_HTML_HREF)
 
 
 def get_fixture_content(path):
     with open(path, 'r') as file:
         return file.read()
-
-
-FAKE_HTML = 'tests/fixtures/root_page.html'
-FAKE_HTML_RESULT = 'tests/fixtures/root_page_correct_result.html'
--- tests/test_root_page.py
+++ tests/test_root_page.py
@@ -4,7 +4,25 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_root_page(
+async def test_open_root_page(
+        client,
+        get_qa_root_html,
+        httpx_mock: HTTPXMock
+):
+    origin_page, result_page = get_qa_root_html
+
+    httpx_mock.add_response(
+        url=ORIGIN_URL,
+        html=origin_page,
+        status_code=200,
+    )
+    resp = await client.get('/')
+    assert resp.status_code == 200
+    resp_data = await resp.get_data(as_text=True)
+    assert resp_data == result_page
+
+@pytest.mark.asyncio
+async def test_symlink_modificataion(
         client,
         get_qa_root_html,
         httpx_mock: HTTPXMock
