01f9596d7c414b38e61028037242bc761b5aee48
yuriy.kormin <yuriy.kormin@gmail.com>
2023-06-07 10:03:32 +0400

    update tests

--- tests/test_parser_module.py
+++ tests/test_parser_module.py
@@ -1,9 +1,16 @@
 from app.parser import process_tags, update_symlinks
 from bs4 import BeautifulSoup
+from app.config import ORIGIN_URL, PROXY_URL
+from urllib.parse import urljoin
+import pytest
 
-
-def test_tags_change(get_qa_root_html):
+@pytest.mark.asyncio
+async def test_tags_change(get_qa_root_html):
     orig_html, result = get_qa_root_html
     soup = BeautifulSoup(orig_html, 'lxml')
-    process_tags(soup)
+    await process_tags(soup)
     assert str(soup) == result
+
+@pytest.mark.asyncio
+async def test_symlink_change(get_qa_root_html):
+    pass
